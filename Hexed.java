7b06dd7cd9eb4b090950ef3a74aba03fc8630975
unknown <2159155@slu.edu.ph>
2018-12-11 00:18:59 +0800

    completed checkPossibleMoves method

--- Hexed.java
+++ Hexed.java
@@ -23,43 +23,52 @@ public class Hexed{
       initialCol = sc.nextInt();
 
       board[initialRow][initialCol] = 'C';
+      initializeBoard(initialRow, initialCol);
 
-      if(initialCol % 2 == 0){
-        if(first == 'r'){
-          board[initialRow + 1][initialCol] = 'r';
-          board[initialRow - 1][initialCol - 1] = 'r';
-          board[initialRow - 1][initialCol + 1] = 'r';
-          board[initialRow - 1][initialCol] = 'g';
-          board[initialRow][initialCol - 1] = 'g';
-          board[initialRow][initialCol + 1] = 'g';
-        }else if (first == 'g'){
-          board[initialRow + 1][initialCol] = 'g'; //above
-          board[initialRow - 1][initialCol - 1] = 'g'; //lower left
-          board[initialRow - 1][initialCol + 1] = 'g'; //lower right
-          board[initialRow - 1][initialCol] = 'r'; //below
-          board[initialRow][initialCol - 1] = 'r'; //upper left
-          board[initialRow][initialCol + 1] = 'r'; //upper right
-        }
-      }else {
-        if(first == 'r'){
-          board[initialRow + 1][initialCol] = 'r';
-          board[initialRow][initialCol - 1] = 'r';
-          board[initialRow][initialCol + 1] = 'r';
-          board[initialRow - 1][initialCol] = 'g';
-          board[initialRow + 1][initialCol - 1] = 'g';
-          board[initialRow + 1][initialCol + 1] = 'g';
-        }else if (first == 'g'){
-          board[initialRow + 1][initialCol] = 'g'; //above
-          board[initialRow][initialCol - 1] = 'g'; //lower left
-          board[initialRow][initialCol + 1] = 'g'; //lower right
-          board[initialRow - 1][initialCol] = 'r'; //below
-          board[initialRow + 1][initialCol - 1] = 'r'; //upper left
-          board[initialRow + 1][initialCol + 1] = 'r'; //upper right
-        }
-      }
+
+
+
 
 
   }
+
+  //initializes the board; puts the starting circle
+  public void initializeBoard(int row, int col){
+    if(initialCol % 2 == 0){
+      if(first == 'r'){
+        board[row + 1][col] = 'r';
+        board[row - 1][col - 1] = 'r';
+        board[row - 1][col + 1] = 'r';
+        board[row - 1][col] = 'g';
+        board[row][col - 1] = 'g';
+        board[row][col + 1] = 'g';
+      }else if (first == 'g'){
+        board[row + 1][col] = 'g'; //above
+        board[row - 1][col - 1] = 'g'; //lower left
+        board[row - 1][col + 1] = 'g'; //lower right
+        board[row - 1][col] = 'r'; //below
+        board[row][col - 1] = 'r'; //upper left
+        board[row][col + 1] = 'r'; //upper right
+      }
+    }else {
+      if(first == 'r'){
+        board[initialRow + 1][initialCol] = 'r';
+        board[initialRow][initialCol - 1] = 'r';
+        board[initialRow][initialCol + 1] = 'r';
+        board[initialRow - 1][initialCol] = 'g';
+        board[initialRow + 1][initialCol - 1] = 'g';
+        board[initialRow + 1][initialCol + 1] = 'g';
+      }else if (first == 'g'){
+        board[initialRow + 1][initialCol] = 'g'; //above
+        board[initialRow][initialCol - 1] = 'g'; //lower left
+        board[initialRow][initialCol + 1] = 'g'; //lower right
+        board[initialRow - 1][initialCol] = 'r'; //below
+        board[initialRow + 1][initialCol - 1] = 'r'; //upper left
+        board[initialRow + 1][initialCol + 1] = 'r'; //upper right
+      }
+    }
+  }
+
   //returns arraylist of coordinates of possible moves
   public ArrayList<Coordinates> checkPossibleMoves(int row, int col, char you, char[][] board){
     ArrayList<Coordinates> enemies = checkEnemyNeighbors(int row, int col, char you, char[][] board);
@@ -68,34 +77,91 @@ public class Hexed{
     for(int i = 0; i < enemies.size(); i++){
 
       if(enemies.get(i).getDirection().equals("above")){
-          int x = 1;
-          while(board[enemies.get(i).getRow() + x][enemies.get(i).getCol()] == opp){
-            x++;
-          }
-          Coordinates possiblemove = new Coordinate(enemies.get(i).getRow() + x, enemies.get(i).getCol(), "above");
-
+        int x = 1;
+        while(board[enemies.get(i).getRow() + x][enemies.get(i).getCol()] == opp){
+          x++;
+        }
+        Coordinates possiblemove = new Coordinate(enemies.get(i).getRow() + x, enemies.get(i).getCol(), "above");
+        moves.add(possiblemove);
       }
 
       if(enemies.get(i).getDirection().equals("lower left")){
-          int x = 1;
-          if(enemies.get(i).getCol() % 2 == 0){
-            while(board[enemies.get(i).getRow() - x][enemies.get(i).getCol() - x] == opp){
-              x++;
-            }
-            Coordinates possiblemove = new Coordinate(enemies.get(i).getRow() - x, enemies.get(i).getCol() - x, "lower left");
-          }else{
-            while(board[enemies.get(i).getRow()][enemies.get(i).getCol() - x] == opp){
-              x++;
-            }
-            Coordinates possiblemove = new Coordinate(enemies.get(i).getRow(), enemies.get(i).getCol() - x, "lower left");
+        int x = 1;
+        if(enemies.get(i).getCol() % 2 == 0){
+          while(board[enemies.get(i).getRow() - x][enemies.get(i).getCol() - x] == opp){
+            x++;
           }
-
-
+          Coordinates possiblemove = new Coordinate(enemies.get(i).getRow() - x, enemies.get(i).getCol() - x, "lower left");
+          moves.add(possiblemove);
+        }else{
+          while(board[enemies.get(i).getRow()][enemies.get(i).getCol() - x] == opp){
+            x++;
+          }
+          Coordinates possiblemove = new Coordinate(enemies.get(i).getRow(), enemies.get(i).getCol() - x, "lower left");
+          moves.add(possiblemove);
+        }
       }
 
+      if(enemies.get(i).getDirection().equals("lower right")){
+        int x = 1;
+        if(enemies.get(i).getCol() % 2 == 0){
+          while(board[enemies.get(i).getRow() - x][enemies.get(i).getCol() + x] == opp){
+            x++;
+          }
+          Coordinates possiblemove = new Coordinate(enemies.get(i).getRow() - x, enemies.get(i).getCol() + x, "lower right");
+          moves.add(possiblemove);
+        }else{
+          while(board[enemies.get(i).getRow()][enemies.get(i).getCol() + x] == opp){
+            x++;
+          }
+          Coordinates possiblemove = new Coordinate(enemies.get(i).getRow(), enemies.get(i).getCol() + x, "lower right");
+          moves.add(possiblemove);
+        }
+      }
 
+      if(enemies.get(i).getDirection().equals("below")){
+        int x = 1;
+        while(board[enemies.get(i).getRow() - x][enemies.get(i).getCol()] == opp){
+          x++;
+        }
+        Coordinates possiblemove = new Coordinate(enemies.get(i).getRow() - x, enemies.get(i).getCol(), "below");
+        moves.add(possiblemove);
+      }
+
+      if(enemies.get(i).getDirection().equals("upper left")){
+        int x = 1;
+        if(enemies.get(i).getCol() % 2 == 0){
+          while(board[enemies.get(i).getRow()][enemies.get(i).getCol() - x] == opp){
+            x++;
+          }
+          Coordinates possiblemove = new Coordinate(enemies.get(i).getRow(), enemies.get(i).getCol() - x, "upper left");
+          moves.add(possiblemove);
+        }else{
+          while(board[enemies.get(i).getRow() + x][enemies.get(i).getCol() - x] == opp){
+            x++;
+          }
+          Coordinates possiblemove = new Coordinate(enemies.get(i).getRow() + x, enemies.get(i).getCol() - x, "upper left");
+          moves.add(possiblemove);
+        }
+      }
+
+      if(enemies.get(i).getDirection().equals("upper right")){
+        int x = 1;
+        if(enemies.get(i).getCol() % 2 == 0){
+          while(board[enemies.get(i).getRow()][enemies.get(i).getCol() + x] == opp){
+            x++;
+          }
+          Coordinates possiblemove = new Coordinate(enemies.get(i).getRow(), enemies.get(i).getCol() + x, "upper left");
+          moves.add(possiblemove);
+        }else{
+          while(board[enemies.get(i).getRow() + x][enemies.get(i).getCol() - x] == opp){
+            x++;
+          }
+          Coordinates possiblemove = new Coordinate(enemies.get(i).getRow() + x, enemies.get(i).getCol() + x, "upper left");
+          moves.add(possiblemove);
+        }
+      }
     }
-
   }
 
   //returns arraylist of enemy coordinates
