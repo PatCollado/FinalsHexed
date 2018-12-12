bfea10cbe773dbe2034bb1a6d478d520b73c1167
unknown <2159155@slu.edu.ph>
2018-12-12 13:47:57 +0800

    started debugging

--- Hexed.java
+++ Hexed.java
@@ -2,21 +2,11 @@ import java.util.*;
 
 public class Hexed{
 
-class Board{
-  char[][] board = new char[7][9];
-  int initialRow = 0;
-  int initialCol = 0;
-
-  public Board(){
-    this.initialRow = initialRow;
-    this.initialCol = initialCol;
-    this.board = board;
-  }
 
 
   //initializes the board; puts the starting circle
-  public void initializeBoard(int row, int col, char first){
-    if(initialCol % 2 == 0){
+  public static void initializeBoard(int row, int col, char first, char[][] board){
+    if(col % 2 == 0){
       if(first == 'r'){
         board[row + 1][col] = 'r';
         board[row - 1][col - 1] = 'r';
@@ -34,24 +24,24 @@ class Board{
       }
     }else {
       if(first == 'r'){
-        board[initialRow + 1][initialCol] = 'r';
-        board[initialRow][initialCol - 1] = 'r';
-        board[initialRow][initialCol + 1] = 'r';
-        board[initialRow - 1][initialCol] = 'g';
-        board[initialRow + 1][initialCol - 1] = 'g';
-        board[initialRow + 1][initialCol + 1] = 'g';
+        board[row + 1][col] = 'r';
+        board[row][col - 1] = 'r';
+        board[row][col + 1] = 'r';
+        board[row - 1][col] = 'g';
+        board[row + 1][col - 1] = 'g';
+        board[row + 1][col + 1] = 'g';
       }else if (first == 'g'){
-        board[initialRow + 1][initialCol] = 'g'; //above
-        board[initialRow][initialCol - 1] = 'g'; //lower left
-        board[initialRow][initialCol + 1] = 'g'; //lower right
-        board[initialRow - 1][initialCol] = 'r'; //below
-        board[initialRow + 1][initialCol - 1] = 'r'; //upper left
-        board[initialRow + 1][initialCol + 1] = 'r'; //upper right
+        board[row + 1][col] = 'g'; //above
+        board[row][col - 1] = 'g'; //lower left
+        board[row][col + 1] = 'g'; //lower right
+        board[row - 1][col] = 'r'; //below
+        board[row + 1][col - 1] = 'r'; //upper left
+        board[row + 1][col + 1] = 'r'; //upper right
       }
     }
   }
-
-  public void printBoard(){
+  // this method prints the initialized board
+  public static void printBoard(char[][] board){
     for (int i = 0; i < board.length; i++){
       for (int j = 0; j < board[i].length; j++){
         System.out.print("[" + board[i][j] + "]");
@@ -60,33 +50,33 @@ class Board{
     }
   }
   //returns list of friendly tiles
-  public ArrayList<Coordinates> scanBoard(char you){
+  public static ArrayList<Coordinates> scanBoard(char you, char[][] board){
     ArrayList<Coordinates> friendlies = new ArrayList<>();
     for(int i = 0; i < board.length; i++){
       for(int j = 0; j < board[i].length; j++){
         if(board[i][j] == you){
-          Coordinates friendly = new Coordinate(i, j, "none");
+          Coordinates friendly = new Coordinates(i, j, "none");
           friendlies.add(friendly);
         }
       }
     }
+    return friendlies;
   }
 
-  public void setMove(int row, int col, char opp){
-    board[row][col] == opp;
+  public static void setMove(int row, int col, char opp, char[][] board){
+    board[row][col] = opp;
   }
-
-}
 
 
   public static void main(String[] args){
       ArrayList<Coordinates> possibleMoves = new ArrayList<>();
       ArrayList<Coordinates> friendlies = new ArrayList<>();
       //ArrayList<Coordinates> bufferPM = new ArrayList<>();
-      Board board;
+      boolean hexChecker;
+      char[][] board = new char[7][9];
       char first;
       char you;
-      char opp;
+      char opp = ' ';
       int initialRow;
       int initialCol;
       int enemyMoveRow;
@@ -110,52 +100,63 @@ class Board{
       initialRow = sc.nextInt();
       initialCol = sc.nextInt();
 
-      board.initializeBoard(initialRow, initialCol, first);
-      board.printBoard();
+      initializeBoard(initialRow, initialCol, first, board);
+      printBoard(board);
 
-      friendlies = scanBoard(you);
+      friendlies = scanBoard(you, board);
       if(first == you){
-        System.out.println("Here are your possible moves:")
-        getPossibleMoves(friendlies, you, opp, board);
+        System.out.println("Here are your possible moves:");
+        getPossibleMoves(friendlies, you, board);
         System.out.println("Please enter your move:");
         yourMoveRow = sc.nextInt();
         yourMoveCol = sc.nextInt();
-        board.setMove(yourMoveRow, yourMoveCol);
+        setMove(yourMoveRow, yourMoveCol, you, board);
         //check for sandwich
 
         System.out.println("What is their move?");
         enemyMoveRow = sc.nextInt();
         enemyMoveCol = sc.nextInt();
-        board.setMove(enemyMoveRow, enemyMoveCol);
+        setMove(enemyMoveRow, enemyMoveCol, opp, board);
         //check for sandwich
       }else {
         System.out.println("What is their move?");
         enemyMoveRow = sc.nextInt();
         enemyMoveCol = sc.nextInt();
-        board.setMove(enemyMoveRow, enemyMoveCol);
+        setMove(enemyMoveRow, enemyMoveCol, opp, board);
         //check for sandwich
         //check if someone won
 
       }
 
       while(winner.equals("none")){
-        System.out.println("Here are your possible moves:")
-        getPossibleMoves(friendlies, you, opp, board);
-        System.out.println("Please enter your move:");
-        yourMoveRow = sc.nextInt();
-        yourMoveCol = sc.nextInt();
-        board.setMove(yourMoveRow, yourMoveCol);
-        //check for sandwich
+        System.out.println("Here are your possible moves:");
+        if(ifHexed(you, board) == true){
+          System.out.println("You are hexed");
+        }else{
+          getPossibleMoves(friendlies, you, board);
+          System.out.println("Please enter your move:");
+          yourMoveRow = sc.nextInt();
+          yourMoveCol = sc.nextInt();
+          setMove(yourMoveRow, yourMoveCol, opp, board);
+          //check for sandwich
+        }
         winner = checkWinner(you, opp, board); //check if someone won
         if(!winner.equals("none")){
             break;
         }
-        System.out.println("What is their move?");
-        enemyMoveRow = sc.nextInt();
-        enemyMoveCol = sc.nextInt();
-        board.setMove(enemyMoveRow, enemyMoveCol);
-        //check for sandwich
+        if(ifHexed(opp, board) == true){
+          System.out.println("They are hexed");
+        }else{
+          System.out.println("What is their move?");
+          enemyMoveRow = sc.nextInt();
+          enemyMoveCol = sc.nextInt();
+          setMove(enemyMoveRow, enemyMoveCol, opp, board);
+          //check for sandwich
+        }
         winner = checkWinner(you, opp, board);//check if someone won
+        if(!winner.equals("none")){
+            break;
+        }
       }
 
       // todo:
@@ -171,7 +172,7 @@ class Board{
 
 
   //returns arraylist of coordinates of possible moves
-  public ArrayList<Coordinates> checkPossibleMoves(int row, int col, char you, char[][] board){
+  public static ArrayList<Coordinates> checkPossibleMoves(int row, int col, char you, char[][] board){
     ArrayList<Coordinates> enemies = checkEnemyNeighbors(row, col, you, board);
     ArrayList<Coordinates> moves = new ArrayList<>();
 
@@ -263,10 +264,12 @@ class Board{
         }
       }
     }
+
+    return moves;
   }
 
   //returns arraylist of enemy coordinates
-  public ArrayList<Coordinates> checkEnemyNeighbors(int row, int col, char you, char[][] board){
+  public static ArrayList<Coordinates> checkEnemyNeighbors(int row, int col, char you, char[][] board){
     ArrayList<Coordinates> coor = new ArrayList<>();
 
     if(col % 2 == 0){
@@ -349,11 +352,11 @@ class Board{
     // board[row + 1][col + 1] upper right
   }
 
-  public String checkWinner(char you, char opp, Board board){
+  public static String checkWinner(char you, char opp, char[][] board){
     ArrayList<Coordinates> enemyMoves = new ArrayList<>();
     ArrayList<Coordinates> friendlyMoves = new ArrayList<>();
-    int enemyMoveCount;
-    int friendlyMoveCount;
+    int enemyMoveCount = 0;
+    int friendlyMoveCount = 0;
     int enemyTiles = 0;
     int friendlyTiles = 0;
     for(int i = 0; i < board.length; i++){
@@ -368,62 +371,173 @@ class Board{
           enemyTiles++;
         }
 
-        if(enemyMoveCount + friendlyMoveCount != 0){
-          return "none";
-        }else{
-          if(enemyTiles > friendlyTiles){
-            return you.charAt(0);
-          }else{
-            return opp.charAt(0);
-          }
-        }
+
+      }
+    }
+    if(enemyMoveCount + friendlyMoveCount != 0){
+      return "none";
+    }else{
+      if(enemyTiles > friendlyTiles){
+        return String.valueOf(you);
+      }else{
+        return String.valueOf(opp);
       }
     }
   }
 
 //transforms sandwiched tiles
-  public void transformSandwiched(int row, int col, char player){
-    ArrayList<Coordinates> pendingTiles = ArrayList<>();
-    int x = 1;
+  public static void transformSandwiched(int row, int col, char player, char[][] board){
+    ArrayList<Coordinates> pendingTiles =  new ArrayList<>();
+
   if (col % 2 == 0){
-    if (board[row + x][col] != player){                //above
+    if (board[row + 1][col] != player){
+      int x = 1;             //above
       while (board[row + x][col] != player){
-        Coordinates pendingCoordinate = new Coordinates(row, col, "none");
+        Coordinates pendingCoordinate = new Coordinates(row + x, col, "none");
         pendingTiles.add(pendingCoordinate);
-        x++
+        x++;
       }
     }
 
-    if (board[row - x][col] != player) {             //below
+    if (board[row - 1][col] != player) {
+      int x = 1;         //below
       while (board[row - x][col] != player){
-        Coordinates pendingCoordinate = new Coordinates(row, col, "none");
+        Coordinates pendingCoordinate = new Coordinates(row - x, col, "none");
         pendingTiles.add(pendingCoordinate);
-        x++
+        x++;
       }
     }
 
-    if (board[row - x][col - x] != player) {
-      while (board[row - x][col - x])
+    if (board[row - 1][col - 1] != player) {
+      int x = 1;     // lowerleft
+      while (board[row - x][col - x] != player){
+        Coordinates pendingCoordinate = new Coordinates(row - x, col - x, "none");
+        pendingTiles.add(pendingCoordinate);
+        x++;
+      }
+    }
+
+    if (board[row - 1][col + 1] != player){
+      int x = 1;     // lowerright
+      while (board[row - x][col + x] != player){
+        Coordinates pendingCoordinate = new Coordinates(row - x, col + x, "none");
+        pendingTiles.add(pendingCoordinate);
+        x++;
+      }
+    }
+
+    if (board[row][col - 1] != player){
+      int x = 1;     // upperleft
+      while (board[row][col - x] != player){
+        Coordinates pendingCoordinate = new Coordinates(row, col - x, "none");
+        pendingTiles.add(pendingCoordinate);
+        x++;
+      }
+    }
+
+    if (board[row][col + 1] != player){
+      int x = 1;     // upperleft
+      while (board[row][col + x] != player){
+        Coordinates pendingCoordinate = new Coordinates(row, col + x, "none");
+        pendingTiles.add(pendingCoordinate);
+        x++;
+      }
     }
   } else {
+    if (board[row + 1][col] != player){
+      int x = 1;             //above
+      while (board[row + x][col] != player){
+        Coordinates pendingCoordinate = new Coordinates(row + x, col, "none");
+        pendingTiles.add(pendingCoordinate);
+        x++;
+      }
+    }
 
+    if (board[row - 1][col] != player){
+      int x = 1;         //below
+      while (board[row - x][col] != player){
+        Coordinates pendingCoordinate = new Coordinates(row - x, col, "none");
+        pendingTiles.add(pendingCoordinate);
+        x++;
+      }
+    }
+
+    if (board[row][col - 1] != player){
+      int x = 1;         //lowerleft
+      while (board[row][col - x] != player){
+        Coordinates pendingCoordinate = new Coordinates(row, col - x, "none");
+        pendingTiles.add(pendingCoordinate);
+        x++;
+      }
+    }
+
+    if (board[row][col + 1] != player){
+      int x = 1;         //lowerright
+      while (board[row][col + x] != player){
+        Coordinates pendingCoordinate = new Coordinates(row, col + x, "none");
+        pendingTiles.add(pendingCoordinate);
+        x++;
+      }
+    }
+
+    if(board[row + 1][col - 1] != player){
+      int x = 1;         //upperleft
+      while (board[row + x][col - x] != player){
+        Coordinates pendingCoordinate = new Coordinates(row + x, col - x, "none");
+        pendingTiles.add(pendingCoordinate);
+        x++;
+      }
+    }
+
+      if(board[row + 1][col + 1] != player){
+        int x = 1;         //upperright
+        while (board[row + x][col + x] != player){
+          Coordinates pendingCoordinate = new Coordinates(row + x, col + x, "none");
+          pendingTiles.add(pendingCoordinate);
+          x++;
+        }
+      }
+  }
+
+  for (int i = 0; i < pendingTiles.size(); i++){
+    board[pendingTiles.get(i).getRow()][pendingTiles.get(i).getCol()] = player;
   }
 
 
 }
 
-  public void setPlayers(char you, char opp){
+public static boolean ifHexed(char player, char[][] board){
+  ArrayList<Coordinates> moves = new ArrayList<>();
+  int numberOfMoves = 0;
+
+  for(int i = 0;i < board.length; i++){
+    for(int j = 0; j < board[i].length; i++){
+      if(board[i][j] == player){
+        moves = checkPossibleMoves(i, j, player, board);
+        numberOfMoves += moves.size();
+      }
+    }
+  }
+
+  if(numberOfMoves == 0){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+  public static void setPlayers(char you, char opp){
     if (you == 'g'){
       opp = 'r';
     } else {
       opp = 'g';
     }
   }
-  public void getPossibleMoves(ArrayList<Coordinates> friendlies, char you, char opp, Board board){
+  public static void getPossibleMoves(ArrayList<Coordinates> friendlies, char player, char[][] board){
     ArrayList<Coordinates> possibleMoves = new ArrayList<>();
     for(int i = 0; i < friendlies.size(); i++){
-            possibleMoves = checkPossibleMoves(friendlies.get(i).getRow, friendlies.get(i).getCol, you, opp, board);
-            System.out.println("Row:" + possibleMoves.get(i).getRow + " Col: " + possibleMoves.get(i).getCol);
+            possibleMoves = checkPossibleMoves(friendlies.get(i).getRow(), friendlies.get(i).getCol(), player, board);
+            System.out.println("Row:" + possibleMoves.get(i).getRow() + " Col: " + possibleMoves.get(i).getCol());
           }
   }
 }
